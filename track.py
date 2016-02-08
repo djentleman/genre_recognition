44b183fbe04643ac068b390144b46a987a32bc3f
Todd Perry <Todd Perry>
2016-02-08 21:12:04 +0100

    improved driectory structure, can now clean up tmp files

--- track.py
+++ track.py
@@ -1,6 +1,7 @@
 # mp3 parsing library
 import eyed3 # used for getting mp3 metadata
-import lastfm
+import utils.lastfm as lastfm
+import utils.wavconvert as wavconvert
 
 class Track:
     def __init__(self, path=""):
@@ -17,14 +18,29 @@ class Track:
             self.getGenreTags()
             
     def getMetadata(self, path):
-        tag = eyed3.load(path).tag
+        self.path = path
+        extension = self.path.split(".")[-1]
+        if extension == "mp3":
+            self.getMetadataMp3()
+        # TODO: metadata getters for wav, flv and ogg
+        
+    def getMetadataMp3(self):
+        tag = eyed3.load(self.path).tag
         self.name = tag.title
         self.artist = tag.artist
         self.album = tag.album
-        self.path = path
 
     def getGenreTags(self):
         self.tags = lastfm.getTrackTags(self.artist, self.name)
+
+    def analyseTrack(self):
+        # first convert to wav
+        if (wavconvert.convertFile(self.path)):
+            # TODO: extract features from the wav file
+            wavconvert.destroyTmp()
+        else:
+            print "Err: File could not be converted into wav"
+        
 
     def dump(self):
         print "Artist: " + self.artist
@@ -40,3 +56,4 @@ if __name__ == "__main__":
     path = "C:\Users\Todd\Music\song.mp3"
     mp3 = Track(path)
     mp3.dump()
+    mp3.analyseTrack()
--- utils/wavconvert.py
+++ utils/wavconvert.py
@@ -1,5 +1,6 @@
 # library for landing wav conversions and storage
 import shutil # for now store the wav file on disk
+import os
 from pydub import AudioSegment  # conversion to wav
 
 
@@ -27,24 +28,33 @@ def canConvert(path):
 
 def convert(path):
     fileFormat = path.split(".")[-1]
+    outpath = "tmp.wav"
     if fileFormat == "wav":
-        moveWav(path, "tmp.wav")
+        moveWav(path, outpath)
     elif fileFormat == "mp3":
-        convertFromMp3(path, "tmp.wav")
+        convertFromMp3(path, outpath)
     elif fileFormat == "ogg":
-        convertFromOgg(path, "tmp.wav")
+        convertFromOgg(path, outpath)
     elif fileFormat == "flv":
-        convertFromFlv(path, "tmp.wav")
+        convertFromFlv(path, outpath)
         
 
-def handleFile(path):
+def convertFile(path):
     if canConvert(path):
         convert(path)
+        return True
+    return False
     # now we can process tmp.wav
+
+def destroyTmp():
+    path = "tmp.wav"
+    os.remove(path)
+    
 
 if __name__ == "__main__":
     path = "C:\Users\Todd\Music\song.mp3"
-    handleFile(path)
+    convertFile(path)
+    destroyTmp()
 
         
     
