d6ff4f54566926c35a576657126897f4497eeaad
Todd Perry <Todd Perry>
2016-02-17 21:46:08 +0100

    now formatting the set of genres

--- csv_interpreter.py
+++ csv_interpreter.py
@@ -1,5 +1,19 @@
 # interpreter for csv file
 
+# for test purposes - this whitelist will be loaded from a config file
+# we can use this to control how many genres we will classify, for instance
+# replacing the whitelist with ['classical', 'metal'] will allow for binary
+# classification
+def getWhitelist():
+    return   ['instrumental hip-hop', 'thrash metal',
+             'chillwave', 'ambient', 'hard rock',
+             'seapunk',
+             'electronica', 'electronic', 'nostalgia', 'Ballad', 'post-trap',
+             'chillout', 'techno',
+             'trip-hop', 'downtempo', 'metallica', 'psychedelic', 'chill',
+             'breakcore', 'metal', 'best track ever',
+             'idm', 'trap', 'rock', 'Acid', 'experimental']
+
 def readCsv(path):
     return [row.split(",") for row in open(path, 'r').read().split("\n")]
 
@@ -11,6 +25,12 @@ def handleCsv(path = "csv.csv"):
     # reformat output vector
     inputVectors = [row[:2495] for row in csv]
     outputData = [row[-5:] for row in csv]
+    uniqueGenres = list(set([elem.lower() for row in outputData for elem in row]))
+    # remove invalid genres
+    whitelist = [genre.lower() for genre in getWhitelist()]
+    filteredGenres = filter(lambda x: x in whitelist, uniqueGenres)
+    print filteredGenres
+    # construct vector from output data
     
 if __name__ == "__main__":
     handleCsv()
