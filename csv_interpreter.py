4be6b3e7bd3dc2d4120e17570ed89ae599221e76
Todd Perry <Todd Perry>
2016-02-17 22:07:35 +0100

    csv interpreter now builds input and output vectors from a csv input

--- csv_interpreter.py
+++ csv_interpreter.py
@@ -9,28 +9,46 @@ def getWhitelist():
              'chillwave', 'ambient', 'hard rock',
              'seapunk',
              'electronica', 'electronic', 'nostalgia', 'Ballad', 'post-trap',
-             'chillout', 'techno',
-             'trip-hop', 'downtempo', 'metallica', 'psychedelic', 'chill',
+             'chillout', 'techno', 'vaporwave',
+             'trip-hop', 'downtempo', 'psychedelic',
              'breakcore', 'metal', 'best track ever',
              'idm', 'trap', 'rock', 'Acid', 'experimental']
 
 def readCsv(path):
     return [row.split(",") for row in open(path, 'r').read().split("\n")]
 
+def getPosition(elem, arr):
+    try:
+        return arr.index(elem)
+    except:
+        return -1
+
 def handleCsv(path = "csv.csv"):
     # load csv into array
     csv = readCsv(path)
     # remove invalid records
     csv = filter(lambda x: len(x) > 2495, csv)
+    # get vectors from csv
+    metadataVector = [row[:3] for row in csv]
+    inputVector = [row[3:2495] for row in csv]
+    outputData = [row[2495:] for row in csv]
     # reformat output vector
-    inputVectors = [row[:2495] for row in csv]
-    outputData = [row[-5:] for row in csv]
     uniqueGenres = list(set([elem.lower() for row in outputData for elem in row]))
     # remove invalid genres
     whitelist = [genre.lower() for genre in getWhitelist()]
     filteredGenres = filter(lambda x: x in whitelist, uniqueGenres)
-    print filteredGenres
     # construct vector from output data
+    outputVector = [[0]*len(filteredGenres) for row in outputData]
+    for i in range(len(outputData)):
+        outputLine = outputData[i]
+        for genre in outputLine:
+            pos = getPosition(genre.lower(), filteredGenres)
+            if pos != -1:
+                outputVector[i][pos] = 1
+    #now we have out vectors, we can hand things over to theano
+    return metadataVector, inputVector, outputVector
+            
+    
     
 if __name__ == "__main__":
     handleCsv()
