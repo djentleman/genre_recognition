10c83533acb4766197d25a5bde022a92605777f2
djentleman <up612136@myport.ac.uk>
2016-02-22 22:14:37 +0000

    updated classificatio util to import sknn: need to increase storage on VPS

--- utils/classification.py
+++ utils/classification.py
@@ -1,13 +1,25 @@
-# wrapper around theano ML library
+# wrapper around skikit ML library
 # exposes wrappers for MLP and SAE
-import theano
+from sknn.mlp import Classifier, Layer
 
-def MultilayerPerceptron():
-	pass
+class ClassificationTools():
+	def __init__(self, inputVector, outputVector):
+		pass
 
-def StackedAutoEncoder():
-	pass
+	def MultilayerPerceptron(self):
+		pass
+
+	def StackedAutoEncoder(self):
+		pass
 
 if __name__ == "__main__":
-	# tests
-	pass
+	# test
+	X_train = [[0, 0], [0, 1], [1, 0], [1, 1]]
+	y_train = [0, 1, 1, 0]
+	nn = Classifier(
+  	  layers=[
+        Layer("Maxout", units=100, pieces=2),
+        Layer("Softmax")],
+   	 learning_rate=0.001,
+    	n_iter=25)
+	nn.fit(X_train, y_train)
